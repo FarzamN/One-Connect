f761c3b23b0204fe78b77c2537dc87b49c724531
FarzamN <89484118+FarzamN@users.noreply.github.com>
2023-02-06 18:24:11 +0500

    splash

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import {} from 'react-native';
+import React, {useEffect} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 import AuthNavigation from './src/navigation/AuthNavigation';
-import {DrawerNavigation} from './src/navigation/DrawerNavigation';
-import UserNavigation from './src/navigation/UserNavigation';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [3000]);
   return <AuthNavigation />;
 };
 
+const styles = StyleSheet.create({});
 export default App;
